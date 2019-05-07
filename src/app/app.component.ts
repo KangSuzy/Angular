858437ba013e2a1b767ece3c258b8fa6ed6c7ced
KangSuzy <astnwl321@gmail.com>
2019-05-07 11:39:00 +0900

    add form and change the designs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
 title = 'Suzy\'s Angular Practice';
+
+
 
 }
 
